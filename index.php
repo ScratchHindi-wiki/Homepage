56ffc51c495c1d3ece7012b7d9ed477be121ca18
Arav Narula <proteximportsexports@gmail.com>
2018-02-19 19:46:35 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+{{:Test Scratch Wiki Home/Home+About}}
+<div style="height:945px">
+<!-- Header -->
+<table align=center style="text-align:center;margin-bottom:8px;" cellpadding=0>
+<tr><td rowspan=2 style="padding-right:25px;">[[File:Scratch_Cat-cropped.png]]</td><td style="font-size:150%; font-weight:bold;">[http://wiki.scratch.mit.edu/wiki/Scratch_Wiki Scratch Wiki]</td><td rowspan=2 style="padding-left:25px;">[[File:Gobo-cropped.png]]</td></tr>
+<tr><td><span style="font-size:100%">Hindi Wiki 
+</table>
+
+<!-- Scratch News -->
+{{Scratchwikiframe|News of scratch|{{:hin:news ofScratch/}}|56%|100px|left|10px|}}<!--
+--><!-- Contents -->
+{{Scratchwikiframe
+|Contents
+|{{hin:Table of Contents}}
+|41%|345px|right|20px
+|hin:Table of Content}}
